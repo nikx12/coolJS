f12eb1e1c7295539d546b0b18e0fa2aa3b6bcf93
nikx12 <nikhitazulka@gmail.com>
2018-12-02 00:17:03 +0530

    Adding code for removing event listeners

--- MemoryMatching/public/script.js
+++ MemoryMatching/public/script.js
@@ -37,21 +37,115 @@ window.onload = function() {
   var numsToAssign = [];
 
   for (let i = 0; i < tilesInput.value / 2; i++) {
-      numsToAssign.push(i, i);
+    numsToAssign.push(i, i);
   }
 
   // Assign each number of `numsToAssign` to each tile randomly
   var randomNum = Math.floor(Math.random() * tilesInput.value);
-  var randomTile = document.querySelectorAll('.tile p')[randomNum];
+  var randomTile = document.querySelectorAll(".tile p")[randomNum];
 
   for (let i = 0; i < tilesInput.value; i++) {
     do {
-        if (!randomTile.innerHTML) {
-            randomTile.innerHTML = numsToAssign.shift();
+      if (!randomTile.innerHTML) {
+        randomTile.innerHTML = numsToAssign.shift();
+      }
+
+      randomNum = Math.floor(Math.random() * tilesInput.value);
+      randomTile = document.querySelectorAll(".tile p")[randomNum];
+    } while (randomTile.innerHTML && numsToAssign.length > 0);
+  }
+
+  function startGame() {
+    let tiles = document.querySelectorAll(".tile");
+    let firstClickedTile;
+    let secondClickedTile;
+
+    // Add click event for every tile
+    for (let i = 0; i < tiles.length; i++) {
+      tiles[i].addEventListener("click", matching);
+    }
+
+    function matching(e) {
+      if (!firstClickedTile) {
+        firstClickedTile = e.target.parentNode;
+        firstClickedTile.firstChild.style.filter = "blur(0px)";
+      } else if (e.target.parentNode !== firstClickedTile) {
+        secondClickedTile = e.target.parentNode;
+        secondClickedTile.firstChild.style.filter = "blur(0px)";
+
+        /*
+            If the tiles match:
+             - add 'matched' class to the matched tiles;
+             - remove the click event for the matched tiles;
+             - remove blur for the matched tiles;
+            Else keep the blur.
+            */
+
+        if (
+          firstClickedTile.firstChild.innerText ==
+          secondClickedTile.firstChild.innerText
+        ) {
+          firstClickedTile.classList.add("matched");
+          secondClickedTile.classList.add("matched");
+
+          firstClickedTile.removeEventListener("click", matching);
+          secondClickedTile.removeEventListener("click", matching);
+
+          firstClickedTile.firstChild.style.filter = "blur(0px)";
+          secondClickedTile.firstChild.style.filter = "blur(0px)";
+
+          tilesClickDelayAndWinCheck();
+        } else {
+          setTimeout(() => {
+            firstClickedTile.firstChild.style.filter = "blur(15px)";
+            secondClickedTile.firstChild.style.filter = "blur(15px)";
+          }, 400);
+
+          tilesClickDelayAndWinCheck();
         }
 
-        randomNum = Math.floor(Math.random() * tilesInput.value);
-        randomTile = document.querySelectorAll('.tile p')[randomNum];
-    } while (randomTile.innerHTML && numsToAssign.length > 0)
-}
+        setTimeout(() => {
+          firstClickedTile = undefined;
+        }, 400);
+      }
+    }
+    // Removes click events on tiles to be able to see the second tile, then adds events back.
+    function tilesClickDelayAndWinCheck() {
+      let notMatchedTiles = 0;
+
+      for (let i = 0; i < tiles.length; i++) {
+        tiles[i].removeEventListener("click", matching);
+
+        if (!tiles[i].classList.contains("matched")) {
+          notMatchedTiles++;
+        }
+      }
+
+      if (notMatchedTiles === 0) {
+        console.log("You won!");
+        winMenu();
+        return;
+      }
+
+      setTimeout(() => {
+        for (let i = 0; i < tiles.length; i++) {
+          tiles[i].addEventListener("click", matching);
+        }
+      }, 400);
+    }
+
+    function winMenu() {
+      body.innerHTML += '<div id="winScreen"></div>';
+      document.querySelector("#winScreen").innerHTML = "<p>You won!</p>";
+
+      setTimeout(() => {
+        document.querySelector("#winScreen").style.background =
+          "rgba(0, 0, 0, .7)";
+      }, 100);
+
+      setTimeout(() => {
+        body.removeChild(document.querySelector("#winScreen"));
+      }, 2000);
+    }
+  }
 };

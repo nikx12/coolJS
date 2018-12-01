3159ecf00aea523ef34691877bfde5b20754a2d2
nikx12 <nikhitazulka@gmail.com>
2018-12-01 22:54:28 +0530

    Adding JS file and modifying code

---
+++ MemoryMatching/public/script.js
@@ -0,0 +1,11 @@
+
+function randomImg(){
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
+var img= document.querySelectorAll(".back-face");
+for(var i= 1; i<=randomNumber;i++){
+
+}
+}
+
+randomImg()

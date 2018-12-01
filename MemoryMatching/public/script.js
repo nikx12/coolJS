1e965479274a1430de775cabfba61cacbc00f089
nikx12 <nikhitazulka@gmail.com>
2018-12-02 00:08:13 +0530

    Adding code for creating new tiles

--- MemoryMatching/public/script.js
+++ MemoryMatching/public/script.js
@@ -17,14 +17,41 @@ window.onload = function() {
     tilesNumber.innerHTML = tilesInput.value;
   });
 
-  resetBtn.addEventListener('click', () => {
-    startBtn.removeAttribute('disabled', '');
-    tilesInput.removeAttribute('disabled', '');
-    playArea.innerHTML = '';
-});
+  resetBtn.addEventListener("click", () => {
+    startBtn.removeAttribute("disabled", "");
+    tilesInput.removeAttribute("disabled", "");
+    playArea.innerHTML = "";
+  });
 
-startBtn.addEventListener('click', () => {
-    startBtn.setAttribute('disabled', '');
-    tilesInput.setAttribute('disabled', '');
-});
+  startBtn.addEventListener("click", () => {
+    startBtn.setAttribute("disabled", "");
+    tilesInput.setAttribute("disabled", "");
+  });
+
+  // Create tiles
+  for (let i = 0; i < tilesInput.value; i++) {
+    playArea.innerHTML += '<div class="tile"><p></p></div>';
+  }
+
+  // Push to `numsToAssign` all the numbers from 0 to `tilesInput / 2`
+  var numsToAssign = [];
+
+  for (let i = 0; i < tilesInput.value / 2; i++) {
+      numsToAssign.push(i, i);
+  }
+
+  // Assign each number of `numsToAssign` to each tile randomly
+  var randomNum = Math.floor(Math.random() * tilesInput.value);
+  var randomTile = document.querySelectorAll('.tile p')[randomNum];
+
+  for (let i = 0; i < tilesInput.value; i++) {
+    do {
+        if (!randomTile.innerHTML) {
+            randomTile.innerHTML = numsToAssign.shift();
+        }
+
+        randomNum = Math.floor(Math.random() * tilesInput.value);
+        randomTile = document.querySelectorAll('.tile p')[randomNum];
+    } while (randomTile.innerHTML && numsToAssign.length > 0)
+}
 };

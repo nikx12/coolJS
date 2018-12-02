59c4e503d3220d3b23884da1cfdcf87fd6eb4996
nikx12 <nikhitazulka@gmail.com>
2018-12-02 19:57:34 +0530

    Changing variables

--- MemoryMatching/public/script.js
+++ MemoryMatching/public/script.js
@@ -7,133 +7,133 @@ window.onload = function() {
   });
 
   const body = document.querySelector("body"),
-    tilesNumber = document.querySelector("#settings p span"),
-    tilesInput = document.querySelector("#settings input"),
+    cardsNumber = document.querySelector("#settings p span"),
+    cardsInput = document.querySelector("#settings input"),
     startBtn = document.querySelector("#settings button[name=start]"),
     resetBtn = document.querySelector("#settings button[name=reset]"),
-    playArea = document.querySelector("#play-area");
+    boardArea = document.querySelector("#board-area");
 
     console.log(startBtn, resetBtn)
 
-  tilesInput.addEventListener("input", () => {
-    tilesNumber.innerHTML = tilesInput.value;
+  cardsInput.addEventListener("input", () => {
+    cardsNumber.innerHTML = cardsInput.value;
   });
 
   resetBtn.addEventListener("click", () => {
     startBtn.removeAttribute("disabled", "");
-    tilesInput.removeAttribute("disabled", "");
-    playArea.innerHTML = "";
+    cardsInput.removeAttribute("disabled", "");
+    boardArea.innerHTML = "";
   });
 
   startBtn.addEventListener("click", () => {
     startBtn.setAttribute("disabled", "");
-    tilesInput.setAttribute("disabled", "");
+    cardsInput.setAttribute("disabled", "");
  
 
-  // Create tiles
-  for (let i = 0; i < tilesInput.value; i++) {
-    playArea.innerHTML += '<div class="tile"><p></p></div>';
+  // Create cards
+  for (let i = 0; i < cardsInput.value; i++) {
+    boardArea.innerHTML += '<div class="card"><p></p></div>';
   }
 
-  // Push to `numsToAssign` all the numbers from 0 to `tilesInput / 2`
+  // Push to `numsToAssign` all the numbers from 0 to `cardsInput / 2`
   var numsToAssign = [];
 
-  for (let i = 0; i < tilesInput.value / 2; i++) {
+  for (let i = 0; i < cardsInput.value / 2; i++) {
     numsToAssign.push(i, i);
   }
 
   // Assign each number of `numsToAssign` to each tile randomly
-  var randomNum = Math.floor(Math.random() * tilesInput.value);
-  var randomTile = document.querySelectorAll(".tile p")[randomNum];
+  var randomNum = Math.floor(Math.random() * cardsInput.value);
+  var randomCard = document.querySelectorAll(".card p")[randomNum];
 
-  for (let i = 0; i < tilesInput.value; i++) {
+  for (let i = 0; i < cardsInput.value; i++) {
     do {
-      if (!randomTile.innerHTML) {
-        randomTile.innerHTML = numsToAssign.shift();
+      if (!randomCard.innerHTML) {
+        randomCard.innerHTML = numsToAssign.shift();
       }
 
-      randomNum = Math.floor(Math.random() * tilesInput.value);
-      randomTile = document.querySelectorAll(".tile p")[randomNum];
-    } while (randomTile.innerHTML && numsToAssign.length > 0);
+      randomNum = Math.floor(Math.random() * cardsInput.value);
+      randomCard = document.querySelectorAll(".card p")[randomNum];
+    } while (randomCard.innerHTML && numsToAssign.length > 0);
   }
   startGame()
 
   });
   function startGame() {
-    let tiles = document.querySelectorAll(".tile");
-    let firstClickedTile;
-    let secondClickedTile;
+    let cards = document.querySelectorAll(".card");
+    let firstClickedCard;
+    let secondClickedCard;
 
     // Add click event for every tile
-    for (let i = 0; i < tiles.length; i++) {
-      tiles[i].addEventListener("click", matching);
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].addEventListener("click", matching);
     }
 
     function matching(e) {
-      if (!firstClickedTile) {
-        firstClickedTile = e.target.parentNode;
-        firstClickedTile.firstChild.style.filter = "blur(0px)";
-      } else if (e.target.parentNode !== firstClickedTile) {
-        secondClickedTile = e.target.parentNode;
-        secondClickedTile.firstChild.style.filter = "blur(0px)";
+      if (!firstClickedCard) {
+        firstClickedCard = e.target.parentNode;
+        firstClickedCard.firstChild.style.filter = "blur(0px)";
+      } else if (e.target.parentNode !== firstClickedCard) {
+        secondClickedCard = e.target.parentNode;
+        secondClickedCard.firstChild.style.filter = "blur(0px)";
 
         /*
-            If the tiles match:
-             - add 'matched' class to the matched tiles;
-             - remove the click event for the matched tiles;
-             - remove blur for the matched tiles;
+            If the cards match:
+             - add 'matched' class to the matched cards;
+             - remove the click event for the matched cards;
+             - remove blur for the matched cards;
             Else keep the blur.
             */
 
         if (
-          firstClickedTile.firstChild.innerText ==
-          secondClickedTile.firstChild.innerText
+          firstClickedCard.firstChild.innerText ==
+          secondClickedCard.firstChild.innerText
         ) {
-          firstClickedTile.classList.add("matched");
-          secondClickedTile.classList.add("matched");
+          firstClickedCard.classList.add("matched");
+          secondClickedCard.classList.add("matched");
 
-          firstClickedTile.removeEventListener("click", matching);
-          secondClickedTile.removeEventListener("click", matching);
+          firstClickedCard.removeEventListener("click", matching);
+          secondClickedCard.removeEventListener("click", matching);
 
-          firstClickedTile.firstChild.style.filter = "blur(0px)";
-          secondClickedTile.firstChild.style.filter = "blur(0px)";
+          firstClickedCard.firstChild.style.filter = "blur(0px)";
+          secondClickedCard.firstChild.style.filter = "blur(0px)";
 
-          tilesClickDelayAndWinCheck();
+          cardsClickDelayAndWinCheck();
         } else {
           setTimeout(() => {
-            firstClickedTile.firstChild.style.filter = "blur(15px)";
-            secondClickedTile.firstChild.style.filter = "blur(15px)";
+            firstClickedCard.firstChild.style.filter = "blur(15px)";
+            secondClickedCard.firstChild.style.filter = "blur(15px)";
           }, 400);
 
-          tilesClickDelayAndWinCheck();
+          cardsClickDelayAndWinCheck();
         }
 
         setTimeout(() => {
-          firstClickedTile = undefined;
+          firstClickedCard = undefined;
         }, 400);
       }
     }
-    // Removes click events on tiles to be able to see the second tile, then adds events back.
-    function tilesClickDelayAndWinCheck() {
-      let notMatchedTiles = 0;
+    // Removes click events on cards to be able to see the second tile, then adds events back.
+    function cardsClickDelayAndWinCheck() {
+      let notMatchedcards = 0;
 
-      for (let i = 0; i < tiles.length; i++) {
-        tiles[i].removeEventListener("click", matching);
+      for (let i = 0; i < cards.length; i++) {
+        cards[i].removeEventListener("click", matching);
 
-        if (!tiles[i].classList.contains("matched")) {
-          notMatchedTiles++;
+        if (!cards[i].classList.contains("matched")) {
+          notMatchedcards++;
         }
       }
 
-      if (notMatchedTiles === 0) {
+      if (notMatchedcards === 0) {
         console.log("You won!");
         winMenu();
         return;
       }
 
       setTimeout(() => {
-        for (let i = 0; i < tiles.length; i++) {
-          tiles[i].addEventListener("click", matching);
+        for (let i = 0; i < cards.length; i++) {
+          cards[i].addEventListener("click", matching);
         }
       }, 400);
     }
